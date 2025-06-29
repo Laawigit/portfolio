014277b70130ce028d9a633788de684c7ad484aa
laawi <abdulhakimsaid665@gmail.com>
2025-06-29 13:00:25 +0300

    replace div with <a> tag in item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 export const Item = ({ project }) => {
   const [mouseOn, setMouseOn] = useState(false);
 
-
   return (
-    <div
+    <a
       onMouseOver={() => setMouseOn(true)}
       onMouseLeave={() => setMouseOn(false)}
       className="w-[85%] max-[768px]:w-[100%] relative overflow-hidden border border-white rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300 h-[15em]"
+      href={project.link}
+      target="_blank"
+      rel="noopener noreferrer"
     >
       <img
         src={project.snapshot}
@@ -20,8 +22,8 @@ export const Item = ({ project }) => {
         } absolute transition-all duration-400 bottom-0 left-0 right-0 bg-cyan-500 opacity-75 text-white flex flex-col items-center justify-center  rounded-2xl max-[768px]:hidden`}
       >
         <span>{project.name}</span>
-        <a href={project.link}>Click here</a>
+        <span>Click here</span>
       </div>
-    </div>
+    </a>
   );
 };
